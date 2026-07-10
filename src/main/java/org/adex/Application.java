1355ffa065bfe344c76c0ac89361341246f8d5ed
unknown <mr.douhi.med@gmail.com>
2026-07-10 20:54:54 +0200

    update main

--- src/main/java/org/adex/Application.java
+++ src/main/java/org/adex/Application.java
@@ -1,8 +1,23 @@
 package org.adex;
 
+import org.adex.server.LoadBalancerServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public interface Application {
 
     static void main() {
+
+        final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+
+        try {
+            LoadBalancerServer server = null;
+            server.start();
+            LOGGER.info("Load balancer started successfully");
+        } catch (Exception e) {
+            LOGGER.error("Failed to start load balancer", e);
+            System.exit(1);
+        }
 
     }
 }
